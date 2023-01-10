db9c7b96cf83c5e746edca95b68edf1ea88c90e9
Haesal <A01376509@itesm.mx>
2023-01-10 13:09:10 -0600

    Pantalla principal con bootstrap

--- music-player/src/App.js
+++ music-player/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>App music, la mejor</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1 className="App-tittle display-1">Music Player</h1>
+          <div className="container">
+              <div className="row">
+                <h2 className="display-6">Listen to your local music</h2>
+              </div>
+              <div className="row">
+                <div className= "col-sm">
+                  <img src={process.env.PUBLIC_URL + "/pant1.png"} alt="image" />
+                  <img src={process.env.PUBLIC_URL + "/pant2.png"} alt="image" />
+                  </div>
+              </div>
+              <div className="row">
+                <div className= "col-sm">
+                  <button type="button" className="btn btn-light">Click to Listen!</button>
+                </div>
+              </div>
+          </div>
+        </div>
       </header>
     </div>
   );
